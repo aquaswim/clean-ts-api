51909d225ab8f8af75b9fe3fe368302a855f79a0
Bayu Setiawan (BS) <bayusetiawan37@gmail.com>
2021-08-16 00:49:17 +0700

    progress teardown

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     setupFiles: ['dotenv-safe/config.js'],
+    globalTeardown: '<rootDir>/__test__/teardown.ts',
 };
--- src/external/mongodb.ts
+++ src/external/mongodb.ts
@@ -4,8 +4,9 @@ import {DBError} from '../commons/Errors';
 const MONGOURI = String(process.env.MONGO_URI);
 const DB_NAME = `${process.env.APP_NAME || 'api'}-db-${process.env.NODE_ENV || 'dev'}`;
 
-// lazy init the mongo database
-let _database: Db;
+// lazy init the mongo database and connection
+let _database: Db | undefined;
+let _client: MongoClient | undefined;
 
 async function getDB(): Promise<Db> {
     try {
@@ -13,9 +14,8 @@ async function getDB(): Promise<Db> {
             return _database;
         }
         // connect to db
-        const client = new MongoClient(MONGOURI);
-        await client.connect();
-        _database = client.db(DB_NAME);
+        _client = await new MongoClient(MONGOURI).connect();
+        _database = _client.db(DB_NAME);
         return _database;
     } catch (err) {
         throw new DBError('mongodb', err.message);
@@ -29,4 +29,17 @@ async function getDB(): Promise<Db> {
 export async function mongodbCollection(name: string) {
     const _db = await getDB();
     return _db.collection(name);
+}
+
+/**
+ * Close Connection
+ * @return {Promise<void>}
+ */
+export async function closeConnection() {
+    if (_client) {
+        await _client.close();
+        _client = undefined;
+        _database = undefined;
+        console.log('DB Closed');
+    }
 }
