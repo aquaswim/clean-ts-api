8fb5ce91775eee7baff62405e69812a717ec01e9
Bayu Setiawan (BS) <bayusetiawan37@gmail.com>
2021-08-17 08:31:30 +0700

    fix unhandled error and change the middleware factory arg to any

--- src/api/commons/create-middleware-decorator.ts
+++ src/api/commons/create-middleware-decorator.ts
@@ -1,19 +1,24 @@
 import {Handler} from 'express';
 
-type HandlerFactory = (...args: unknown[]) => Handler;
+type HandlerFactory = (...args: any[]) => Handler;
 
 export function createMiddlewareMethodDecorator(middleware: Handler) {
     return function () {
         return function (target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<any>) {
             const originalHandler = descriptor.value! as Handler;
             const newHandler: Handler = async function (req, res, next) {
-                // this is stupid, and needed since the route decorator always expect promise handler
+                // this is stupid, and i don;t know why
                 await middleware(req, res, async err => {
-                    // and i still don't understand why this works
                     if (err) {
                         return next(err);
                     }
-                    await originalHandler(req, res, next);
+                    // this is stupid, the original handler need to have it try catch block, since all throw in the
+                    // original handler will result in unhandled promise error
+                    try {
+                        await originalHandler(req, res, next);
+                    } catch (err) {
+                        next(err);
+                    }
                 });
             };
             descriptor.value = newHandler;
@@ -24,6 +29,6 @@ export function createMiddlewareMethodDecorator(middleware: Handler) {
 export function createMiddlewareFactoryMethodDecorator(mf: HandlerFactory) {
     return function (...args: unknown[]) {
         const middleware = mf(args);
-        return createMiddlewareMethodDecorator(middleware);
+        return createMiddlewareMethodDecorator(middleware)();
     };
 }
